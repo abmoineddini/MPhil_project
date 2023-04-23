0e170b988b733d08abe27beae77d5d78355ed379
Amirbahador Moineddini <abmoineddini@gmail.com>
2023-04-23 19:45:21 +0100

    Adding preprocessing

--- SpeechRec/Speech_preprocessing.py
+++ SpeechRec/Speech_preprocessing.py
@@ -9,6 +9,7 @@ import os
 from os.path import isdir
 from scipy.fft import fft, fftfreq
 import random
+from random import randint
 
 
 def SepWithSTFTAuto(data, time, Fs):
@@ -395,3 +396,76 @@ def Start_spatial_preprocessing(self, DataSetName):
                 os.remove(FigureName)
 
 
+def PreprocessingMeth2(data, FolderSTFTTraining, FolderSTFTTesting, dataFileName):
+    time = data[0:len(data)-4000, 1]
+    voltage = data[0:len(data)-4000, 2]
+    Fs = 4000
+
+    TrainingDirectory = [f for f in listdir(FolderSTFTTraining)]
+    TestDirectory = [f for f in listdir(FolderSTFTTesting)]
+    detenV = sig.detrend(voltage)
+
+    filter = sig.butter(2, [95, 1500], 'bandpass', fs=4000, output='sos')
+    corrVoltage = sig.sosfilt(filter, detenV)
+    corrVoltage = (corrVoltage*5)/1023
+
+    maxV = max(corrVoltage)
+    normV = corrVoltage/maxV
+
+    countTrain = int(len(TrainingDirectory))
+    countTest = int(len(TestDirectory))
+
+    fig0 = plt.figure()
+    ax0 = plt.Axes(fig0, [0., 0., 1., 1.])
+    plt.style.use('dark_background')
+    plt.scatter(time, normV, c=abs(normV * normV), s=abs(normV))
+    plt.gray()
+    plt.axis('off')
+    ax0.set_axis_off()
+    plt.tight_layout()
+    plt.show()
+    fig0.savefig('ScattTestingFigure.png', bbox_inches='tight', pad_inches=0)
+
+    f1, t1, Zxx1 = sig.stft(normV, Fs, nperseg=1000)
+    fig1 = plt.figure(frameon=False)
+    ax1 = plt.Axes(fig1, [0., 0., 1., 1.])
+    plt.pcolormesh(t1, f1, np.abs(Zxx1), shading='gouraud', cmap='gray')
+    plt.axis('off')
+    plt.ylim([90, 500])
+    ax1.set_axis_off()
+    plt.tight_layout()
+    plt.show()
+    fig1.savefig('STFTTestingFigure.png', bbox_inches='tight', pad_inches=0)
+
+    Training = [1, 3, 4]
+
+    from PIL import Image
+    def ProcessIm(Im1, Im2, fileName):
+        image1 = Image.open(Im1)
+        image1 = image1.rotate(90)
+
+        image2 = Image.open(Im2)
+        image1_size = image1.size
+        new_image = Image.new('RGB', (2 * image1_size[0], image1_size[1]), (250, 250, 250))
+        new_image.paste(image1, (0, 0))
+        new_image.paste(image2, (image1_size[0], 0))
+        new_image.save(fileName)
+
+
+    i = randint(1, 4)
+    if i in Training:
+        nameTrain = str(countTrain)
+        countTrain += 1
+        FileName = FolderSTFTTraining + "/" + nameTrain + '.png'
+        print(FileName)
+        ProcessIm('STFTTestingFigure.png', 'ScattTestingFigure.png', FileName)
+
+    else:
+        nameTest = str(countTest)
+        countTest += 1
+        FileName = FolderSTFTTesting + "/" + nameTest + '.png'
+        print(FileName)
+        ProcessIm('STFTTestingFigure.png', 'ScattTestingFigure.png', FileName)
+
+    os.remove("STFTTestingFigure.png")
+    os.remove("ScattTestingFigure.png")
